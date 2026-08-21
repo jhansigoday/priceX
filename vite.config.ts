7749a796b0f2ede093010347cb69995e8ee78019
jhansigoday <jhansigoday@ gmail.com>
2026-08-21 12:19:52 +0530

    Initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -106,7 +106,7 @@ export default defineConfig(({ mode }) => ({
       });
     }
   },
-  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+  plugins: [react()].filter(Boolean),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
